7288ca06ed9a7d49f62e6a6e42e5c6c10279466f
Matt Venn <matt@mattvenn.net>
2014-02-17 18:19:18 +0000

    tidied up for library

--- driver.py
+++ driver.py
@@ -1,17 +1,18 @@
-#import the library to control the GPIO pins
+import time
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-#import the time library
-import time
+#verbose output
+verbose = False
 
-not_oe = 8
-le = 10
-clk = 12
-sdo = 16
+#pin defs
+not_oe = 8 #output enable
+le = 10 #latch
+clk = 12 #clock
+sdo = 16 #data out
 
-#set the pin to be an output
+#set the pins to be outputs
 GPIO.setup(not_oe, GPIO.OUT)
 GPIO.setup(le, GPIO.OUT)
 GPIO.setup(clk, GPIO.OUT)
@@ -24,6 +25,8 @@ GPIO.output(le,False)
 #clock starts low
 GPIO.output(clk,False)
 
+#define which segments are on/off for each digit
+#last element is for the point
 digits = [
  [ 1,1,1,1,1,1,0,0 ], # 0
  [ 0,1,1,0,0,0,0,0 ], # 1
@@ -36,18 +39,8 @@ digits = [
  [ 1,1,1,1,1,1,1,0 ], # 8
  [ 1,1,1,0,0,1,1,0 ], # 9
 ]
-"""
-digits = [
- [ 1,0,0,0,0,0,0,0 ], # 0
- [ 0,1,0,0,0,0,0,0 ], # 0
- [ 0,0,1,0,0,0,0,0 ], # 0
- [ 0,0,0,1,0,0,0,0 ], # 0
- [ 0,0,0,0,1,0,0,0 ], # 0
- [ 0,0,0,0,0,1,0,0 ], # 0
- [ 0,0,0,0,0,0,1,0 ], # 0
- [ 0,0,0,0,0,0,0,1 ], # 0
-]
-"""
+
+#do the work
 def send_digit(digit,point=False):
     GPIO.output(not_oe, True)
 
@@ -55,7 +48,9 @@ def send_digit(digit,point=False):
         digits[digit][7] = 1
     else:
         digits[digit][7] = 0
-    print("sending:", digit, " = ", digits[digit])
+
+    if verbose:
+        print("sending:", digit, " = ", digits[digit])
 
     #next 7 clock pulses
     for i in range(8):
@@ -78,12 +73,22 @@ def send_digit(digit,point=False):
     #turn on leds
     GPIO.output(not_oe, False)
 
-point = False
-while True:
-    for i in range(10):
-        send_digit(i,point)
-        time.sleep(1.5)
-    if point:
-        point = False
-    else:
-        point = True
+def turn_off():
+    GPIO.output(not_oe, True)
+
+def turn_on():
+    GPIO.output(not_oe, False)
+
+#run a test sequence
+if __name__ == '__main__':
+    print("running test sequence")
+    point = False
+    while True:
+        for i in range(10):
+            print("sending %d%s" % (i, "." if point else ""))
+            send_digit(i,point)
+            time.sleep(0.25)
+        if point:
+            point = False
+        else:
+            point = True
