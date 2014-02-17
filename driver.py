7b7680e98319e91004395f1029a07b2121d726c9
Matt Venn <matt@mattvenn.net>
2014-02-17 22:46:24 +0000

    handle multiple displays, untested

--- driver.py
+++ driver.py
@@ -1,10 +1,20 @@
+"""
+title: big 7 segment driver
+author: matt venn, 2014
+license: GPL attribution share alike
+"""
 import time
-import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
 
 #verbose output
-verbose = False
+verbose = True
+
+#undefine for testing with GPIO lib
+raspi = True
+
+if raspi:
+    import RPi.GPIO as GPIO
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
 
 #pin defs
 not_oe = 8 #output enable
@@ -13,17 +23,18 @@ clk = 12 #clock
 sdo = 16 #data out
 
 #set the pins to be outputs
-GPIO.setup(not_oe, GPIO.OUT)
-GPIO.setup(le, GPIO.OUT)
-GPIO.setup(clk, GPIO.OUT)
-GPIO.setup(sdo, GPIO.OUT)
+if raspi:
+    GPIO.setup(not_oe, GPIO.OUT)
+    GPIO.setup(le, GPIO.OUT)
+    GPIO.setup(clk, GPIO.OUT)
+    GPIO.setup(sdo, GPIO.OUT)
 
-#turn off leds
-GPIO.output(not_oe, True)
-#turn off latch
-GPIO.output(le,False)
-#clock starts low
-GPIO.output(clk,False)
+    #turn off leds
+    GPIO.output(not_oe, True)
+    #turn off latch
+    GPIO.output(le,False)
+    #clock starts low
+    GPIO.output(clk,False)
 
 #define which segments are on/off for each digit
 #last element is for the point
@@ -40,55 +51,71 @@ digits = [
  [ 1,1,1,0,0,1,1,0 ], # 9
 ]
 
-#do the work
-def send_digit(digit,point=False):
-    GPIO.output(not_oe, True)
+#sends a string representation of a float, deals with floating points
+def update(number):
+    #turn leds off before update
+    turn_off()
+    point = False
 
+    #send least significant digit first
+    for char in reversed(number):
+        if char == '.':
+            point = True
+            continue
+        send_digit(int(char),point)
+        point = False
+
+    #latch the outputs
+    if raspi:
+        GPIO.output(le,True)
+        GPIO.output(le,False)
+    
+    #turn on leds
+    turn_on()
+
+#send a number, with optional point
+def send_digit(digit,point=False):
+    #set the point bit
     if point:
         digits[digit][7] = 1
     else:
         digits[digit][7] = 0
 
     if verbose:
-        print("sending:", digit, " = ", digits[digit])
+        print("sending %d%s = %s" % (
+            digit,
+            '.' if point else '',
+            ','.join(str(x) for x in digits[digit]))
+            )
 
-    #next 7 clock pulses
-    for i in range(8):
-        GPIO.output(clk,False)
-        #data
-        if digits[digit][7-i]:
-            GPIO.output(sdo,True)
-        else:
-            GPIO.output(sdo,False)
+    #8 clock pulses
+    if raspi:
+        for i in range(8):
+            GPIO.output(clk,False)
+            #data
+            if digits[digit][7-i]:
+                GPIO.output(sdo,True)
+            else:
+                GPIO.output(sdo,False)
 
-        GPIO.output(clk,True)
+            GPIO.output(clk,True)
 
-
-    GPIO.output(clk,True)
-
-    #latch
-    GPIO.output(le,True)
-    GPIO.output(le,False)
-
-    #turn on leds
-    GPIO.output(not_oe, False)
-
+#turn off leds
 def turn_off():
-    GPIO.output(not_oe, True)
+    if raspi:
+        GPIO.output(not_oe, True)
 
+#turn on leds
 def turn_on():
-    GPIO.output(not_oe, False)
+    if raspi:
+        GPIO.output(not_oe, False)
 
 #run a test sequence
 if __name__ == '__main__':
     print("running test sequence")
     point = False
     while True:
-        for i in range(10):
-            print("sending %d%s" % (i, "." if point else ""))
-            send_digit(i,point)
-            time.sleep(0.25)
-        if point:
-            point = False
-        else:
-            point = True
+        for i in range(100):
+            print("sending %.2f" % (i/100.0))
+            time.sleep(0.05)
+            update("%.2f" % (i/100.0))
