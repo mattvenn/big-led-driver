fc290bf6f7ca9107ebb0eb6f81965e21ffc20013
matt venn <matt@mattvenn.net>
2014-02-27 11:11:18 +0000

    switched to char instead of digit, took out scrolling

--- driver.py
+++ driver.py
@@ -5,29 +5,38 @@ license: GPL attribution share alike
 """
 import time
 
-#verbose output
-verbose = True
-
-#how long to wait before next char sent
-scroll_time = 1.0
-BLANK = 10
-
 #undefine for testing with GPIO lib
 raspi = True
+
+#define which segments are on/off for each digit
+#last element is for the point
+letters = {
+ '0' : [ 1,1,1,1,1,1,0,0 ], # 0
+ '1' : [ 0,1,1,0,0,0,0,0 ], # 1
+ '2' : [ 1,1,0,1,1,0,1,0 ], # 2
+ '3' : [ 1,1,1,1,0,0,1,0 ], # 3
+ '4' : [ 0,1,1,0,0,1,1,0 ], # 4
+ '5' : [ 1,0,1,1,0,1,1,0 ], # 5
+ '6' : [ 0,0,1,1,1,1,1,0 ], # 6
+ '7' : [ 1,1,1,0,0,0,0,0 ], # 7
+ '8' : [ 1,1,1,1,1,1,1,0 ], # 8
+ '9' : [ 1,1,1,0,0,1,1,0 ], # 9
+ ' ' : [ 0,0,0,0,0,0,0,0 ], 
+ '.' : [ 0,0,0,0,0,0,0,1 ], 
+ 'P' : [ 1,1,0,0,1,1,1,1 ], 
+}
 
 if raspi:
     import RPi.GPIO as GPIO
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
 
-#pin defs
-not_oe = 8 #output enable
-le = 10 #latch
-clk = 12 #clock
-sdo = 16 #data out
+    #pin defs
+    not_oe = 8 #output enable
+    le = 10 #latch
+    clk = 12 #clock
+    sdo = 16 #data out
 
-#set the pins to be outputs
-if raspi:
     GPIO.setup(not_oe, GPIO.OUT)
     GPIO.setup(le, GPIO.OUT)
     GPIO.setup(clk, GPIO.OUT)
@@ -40,113 +49,100 @@ if raspi:
     #clock starts low
     GPIO.output(clk,False)
 
-#define which segments are on/off for each digit
-#last element is for the point
-digits = [
- [ 1,1,1,1,1,1,0,0 ], # 0
- [ 0,1,1,0,0,0,0,0 ], # 1
- [ 1,1,0,1,1,0,1,0 ], # 2
- [ 1,1,1,1,0,0,1,0 ], # 3
- [ 0,1,1,0,0,1,1,0 ], # 4
- [ 1,0,1,1,0,1,1,0 ], # 5
- [ 0,0,1,1,1,1,1,0 ], # 6
- [ 1,1,1,0,0,0,0,0 ], # 7
- [ 1,1,1,1,1,1,1,0 ], # 8
- [ 1,1,1,0,0,1,1,0 ], # 9
- [ 0,0,0,0,0,0,0,0 ], # all off
-]
+class driver:
+    BLANK = 10
+    def __init__(self):
+        #verbose output
+        self.verbose = True
 
-#sends a string representation of a float, deals with floating points
-def update(number,scroll=False):
-    #turn leds off before update
-#    turn_off()
-    point = False
-    
-    if scroll:
-        for char in number:
-            if char == '.':
-                send_digit(BLANK,True)
-            else:
-                send_digit(int(char),False)
+        freq = 200 #hz
+        self.pwm = GPIO.PWM(not_oe, freq)
+        #lights off to start with
+        self.pwm.start(100) #duty cycle
 
-            #latch the outputs
-            if raspi:
-                GPIO.output(le,True)
-                GPIO.output(le,False)
-
-            time.sleep(scroll_time)
-
-    else:
+    #sends a string representation of a float, deals with floating points
+    def update(self,number):
+        point = False
+        
         #send least significant digit first
         for char in reversed(number):
             if char == '.':
                 point = True
                 continue
-            send_digit(int(char),point)
+            self.send_digit(char,point)
             point = False
 
         #latch the outputs
         if raspi:
             GPIO.output(le,True)
             GPIO.output(le,False)
-    
+        
+
+    #send a char, with optional point
+    def send_digit(self,char,point=False):
+        #set the point bit
+        if point:
+            letters[char][7] = 1
+        else:
+            letters[char][7] = 0
+
+        if self.verbose:
+            print("sending %s%s = %s" % (
+                char,
+                '.' if point else '',
+                ','.join(str(x) for x in letters[char]))
+                )
+
+        #8 clock pulses
+        if raspi:
+            for i in range(8):
+                GPIO.output(clk,False)
+                #data
+                if letters[char][7-i]:
+                    GPIO.output(sdo,True)
+                else:
+                    GPIO.output(sdo,False)
+
+                GPIO.output(clk,True)
+
+    #turn off leds
+    def turn_off(self):
+        if raspi:
+            self.set_pwm(0)
+
     #turn on leds
-#    turn_on()
+    def turn_on(self):
+        if raspi:
+            self.set_pwm(100)
 
-#send a number, with optional point
-def send_digit(digit,point=False):
-    #set the point bit
-    if point:
-        digits[digit][7] = 1
-    else:
-        digits[digit][7] = 0
 
-    if verbose:
-        print("sending %d%s = %s" % (
-            digit,
-            '.' if point else '',
-            ','.join(str(x) for x in digits[digit]))
-            )
+    def set_pwm(self,pwm):
+        self.pwm.ChangeDutyCycle(100-pwm)
 
-    #8 clock pulses
-    if raspi:
-        for i in range(8):
-            GPIO.output(clk,False)
-            #data
-            if digits[digit][7-i]:
-                GPIO.output(sdo,True)
-            else:
-                GPIO.output(sdo,False)
+    def fade(self,start,end,length):
+        if start > end:
+           step = -1
+        else:
+            step = 1
+        for i in range(start,end + step,step):
+            time.sleep(float(length) / abs(start-end))
+            self.set_pwm(i)
 
-            GPIO.output(clk,True)
+    def cleanup(self):
+        self.set_pwm(0)
+        GPIO.cleanup()
+        #cleanup
 
-#turn off leds
-def turn_off():
-    if raspi:
-        GPIO.output(not_oe, True)
-
-#turn on leds
-def turn_on():
-    if raspi:
-        GPIO.output(not_oe, False)
-
-def init_pwm():
-    p = GPIO.PWM(not_oe, 50)
-    p.start(1)
-    return p
-
-def set_pwm(p,pwm):
-    p.ChangeDutyCycle(pwm)
 
 #run a test sequence
 if __name__ == '__main__':
+    driver = driver()
+    driver.set_pwm(100)
     print("running test sequence")
     point = False
-#    turn_on()
-    p = init_pwm()
-    set_pwm(p,50)
     while True:
-        for i in range(100):
-            print("sending %.1f" % (i/10.0))
-            time.sleep(0.10)
-            update("%02d" % (i))
+        for i in range(1000):
+            string = "%.2f" % (i/100.0)
+            print("sending %s" % string)
+            driver.update(string)
+            time.sleep(0.05)
